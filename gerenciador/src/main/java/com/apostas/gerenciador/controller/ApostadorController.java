f8ca5d3599df85ef60b05490d1533902155b045b
Alan Araujo Rodrigues <88983038+alantrs@users.noreply.github.com>
2023-07-19 23:31:10 -0300

    Metodo de salvar apostador criado na camada de servico e sendo chamada na camada de controlador

--- gerenciador/src/main/java/com/apostas/gerenciador/controller/ApostadorController.java
+++ gerenciador/src/main/java/com/apostas/gerenciador/controller/ApostadorController.java
@@ -1,9 +1,28 @@
 package com.apostas.gerenciador.controller;
 
+import com.apostas.gerenciador.model.Apostador;
+import com.apostas.gerenciador.model.record.DadosCadastroApostador;
+import com.apostas.gerenciador.service.ApostadorService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/apostador")
 public class ApostadorController {
+
+    @Autowired
+    private ApostadorService apostadorService;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Apostador> salvarApostador(@RequestBody DadosCadastroApostador dadosCadastroApostador){
+        Apostador apostador = apostadorService.salvarApostador(dadosCadastroApostador);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apostador);
+    }
 }
--- gerenciador/src/main/java/com/apostas/gerenciador/service/ApostadorService.java
+++ gerenciador/src/main/java/com/apostas/gerenciador/service/ApostadorService.java
@@ -1,7 +1,19 @@
 package com.apostas.gerenciador.service;
 
+import com.apostas.gerenciador.model.Apostador;
+import com.apostas.gerenciador.model.record.DadosCadastroApostador;
+import com.apostas.gerenciador.repository.ApostadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApostadorService {
+
+    @Autowired
+    private ApostadorRepository apostadorRepository;
+    public Apostador salvarApostador(DadosCadastroApostador dadosCadastroApostador){
+        Apostador apostador = new Apostador(dadosCadastroApostador);
+        apostadorRepository.save(apostador);
+        return apostador;
+    }
 }
