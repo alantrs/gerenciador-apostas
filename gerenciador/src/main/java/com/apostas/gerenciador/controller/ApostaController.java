e62e7ba5c56d3cf066bd635bff70e0537f6c1bcc
Alan Araujo Rodrigues <88983038+alantrs@users.noreply.github.com>
2023-07-26 15:18:25 -0300

    metodo para listar apostas

--- gerenciador/src/main/java/com/apostas/gerenciador/controller/ApostaController.java
+++ gerenciador/src/main/java/com/apostas/gerenciador/controller/ApostaController.java
@@ -1,5 +1,6 @@
 package com.apostas.gerenciador.controller;
 
+import com.apostas.gerenciador.model.Aposta;
 import com.apostas.gerenciador.model.record.DadosCadastroAposta;
 import com.apostas.gerenciador.model.record.DadosListagemAposta;
 import com.apostas.gerenciador.service.ApostaService;
@@ -7,6 +8,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/aposta")
@@ -18,8 +21,14 @@ public class ApostaController {
     @PostMapping
     @Transactional
     public ResponseEntity<DadosListagemAposta> realizarAposta(@RequestBody DadosCadastroAposta dadosCadastroAposta){
-        var aposta = apostaService.realizarAposta(dadosCadastroAposta);
+        DadosListagemAposta aposta = apostaService.realizarAposta(dadosCadastroAposta);
         return ResponseEntity.ok().body(aposta);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DadosListagemAposta>> listarApostas(){
+        List<DadosListagemAposta> apostas = apostaService.listarApostas();
+        return ResponseEntity.ok().body(apostas);
     }
 
 }
--- gerenciador/src/main/java/com/apostas/gerenciador/service/ApostaService.java
+++ gerenciador/src/main/java/com/apostas/gerenciador/service/ApostaService.java
@@ -4,10 +4,12 @@ import com.apostas.gerenciador.model.Aposta;
 import com.apostas.gerenciador.model.Apostador;
 import com.apostas.gerenciador.model.record.DadosCadastroAposta;
 import com.apostas.gerenciador.model.record.DadosListagemAposta;
+import com.apostas.gerenciador.repository.ApostaRepository;
 import com.apostas.gerenciador.repository.ApostadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,13 +18,21 @@ public class ApostaService {
 
     @Autowired
     private ApostadorRepository apostadorRepository;
+    @Autowired
+    private ApostaRepository apostaRepository;
 
     public DadosListagemAposta realizarAposta(DadosCadastroAposta dadosCadastroAposta){
         Optional<Apostador> apostadorEncontrado = apostadorRepository.findById(dadosCadastroAposta.idApostador());
         String numeroAposta = UUID.randomUUID().toString();
 
         Aposta aposta = new Aposta(dadosCadastroAposta, numeroAposta);
+        apostaRepository.save(aposta);
 
         return new DadosListagemAposta(aposta, apostadorEncontrado.get());
+    }
+
+    public List<DadosListagemAposta> listarApostas() {
+        List<Aposta> apostas = apostaRepository.findAll();
+        return apostas.stream().map(aposta -> (new DadosListagemAposta(aposta, aposta.getApostador()))).toList();
     }
 }
